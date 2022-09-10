b841f246403a40cf074df47c96ae5c8e6698f782
Yjn024 <jiening.yu@outlook.com>
2022-09-10 21:06:40 +0800

    flagbox

--- src/main/java/com/dm/earth/cabricality/mixin/FlagBlockMixin.java
+++ src/main/java/com/dm/earth/cabricality/mixin/FlagBlockMixin.java
@@ -1,8 +1,43 @@
 package com.dm.earth.cabricality.mixin;
 
+import com.dm.earth.cabricality.util.VoxelShapeUtil;
 import com.github.alexnijjar.ad_astra.blocks.flags.FlagBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FlagBlock.class)
 public class FlagBlockMixin {
+
+    @Shadow @Final public static DirectionProperty FACING;
+    @Shadow @Final public static EnumProperty<DoubleBlockHalf> HALF;
+
+    @Inject(method = "getOutlineShape", at = @At("HEAD"), cancellable = true)
+    private void injectedOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
+        Direction direction = state.get(FACING);
+        if (state.get(HALF).equals(DoubleBlockHalf.LOWER)) {
+            cir.setReturnValue(VoxelShapes.union(
+                    VoxelShapeUtil.simpleBox(direction, 0, 0, 7, 5, 16, 9),
+                    VoxelShapeUtil.simpleBox(direction, 5, 0, 7.5, 13, 16, 8.5)
+            ));
+        } else {
+            cir.setReturnValue(VoxelShapes.union(
+                    VoxelShapeUtil.simpleBox(direction, 0, 0, 7, 5, 14, 9),
+                    VoxelShapeUtil.simpleBox(direction, 5, 0, 7.5, 13, 15, 8.5)
+            ));
+        }
+    }
 }
---
+++ src/main/java/com/dm/earth/cabricality/util/VoxelShapeUtil.java
@@ -0,0 +1,33 @@
+package com.dm.earth.cabricality.util;
+
+import com.github.alexnijjar.ad_astra.blocks.flags.FlagBlock;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
+
+public class VoxelShapeUtil {
+    public static VoxelShape simpleBox(double x1, double y1, double z1, double x2, double y2, double z2) {
+        return FlagBlock.boxSimple(x1, y1, z1, x2, y2, z2);
+    }
+
+    //input: north
+    public static VoxelShape simpleBox(Direction direction, double x1, double y1, double z1, double x2, double y2, double z2) {
+        VoxelShape returnBox = simpleBox(x1, y1, z1, x2, y2, z2);
+        switch (direction) {
+            case NORTH -> {
+                returnBox = simpleBox(x1, y1, z1, x2, y2, z2);
+            }
+            case EAST -> {
+                returnBox = simpleBox(z1, y1, 16-x1, z2, y2, 16-x2);
+            }
+            case SOUTH -> {
+                returnBox = simpleBox(16-x1, y1, 16-z1, 16-x2, y2, 16-z2);
+            }
+            case WEST -> {
+                returnBox = simpleBox(16-z1, y1, x1, 16-z2, y2, x2);
+            }
+        }
+        return returnBox;
+    }
+}
