1877b7719a9b2c9f6f949889febbad68e0896324
Yjn024 <jiening.yu@outlook.com>
2022-09-11 10:30:43 +0800

    simple datafixers

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ A small mod for [Cabricality](https://github.com/DM-Earth/Cabricality) Modpack.
 ## Functions
 
 - Make Trade Cards and Profession Cards easier to translate.
+- Let Create's wrench replace Ad Astra's wrench.
+- Tweak bounding boxes of Flags from Ad Astra to match the custom model in Cabricality.
 
 When certain necessary mods are not loaded:
 
---
+++ src/main/java/com/dm/earth/cabricality/mixin/SchemasMixin.java
@@ -0,0 +1,37 @@
+package com.dm.earth.cabricality.mixin;
+
+import com.dm.earth.cabricality.util.DataFixerUtil;
+import com.google.common.collect.ImmutableMap;
+import com.mojang.datafixers.DataFixerBuilder;
+import com.mojang.datafixers.schemas.Schema;
+import net.minecraft.datafixer.Schemas;
+import net.minecraft.datafixer.fix.ItemNameFix;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.UnaryOperator;
+
+@Mixin(Schemas.class)
+public class SchemasMixin {
+    @Shadow @Final private static BiFunction<Integer, Schema, Schema> EMPTY_IDENTIFIER_NORMALIZE;
+
+    @Shadow
+    private static UnaryOperator<String> replacing(Map<String, String> replacements) {
+        return null;
+    }
+
+    @Inject(method = "build", at = @At("TAIL"))
+    private static void CabricalitySchemasDFU(DataFixerBuilder builder, CallbackInfo ci) {
+        ImmutableMap<String, String> map = DataFixerUtil.read();
+        if (!map.isEmpty()) {
+            Schema schemaCabricality = builder.addSchema(114514, EMPTY_IDENTIFIER_NORMALIZE);
+            builder.addFixer(ItemNameFix.create(schemaCabricality, "Cabricality DataFixers", replacing(map)));
+        }
+    }
+}
